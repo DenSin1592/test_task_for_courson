83c9c8e13a07e12af95be1dc1862c65fa8bc70b3
Denis Lyubavin <lyubavin.denis@diol-it.ru>
2022-12-05 01:10:04 +0300

    правки

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,16 +2,13 @@
 
 namespace App\Repositories;
 
-use App\Models\Contact;
 use App\Models\User;
 use App\Repositories\Core\AbstractRepository;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
+
 
 
 final class UserRepository extends AbstractRepository
 {
-
     protected function setModel(): void
     {
         $this->model = new User();
